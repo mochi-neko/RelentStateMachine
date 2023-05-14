87a0ef1c06ddcdf6b086ce8ca83e02a2a72ddfd1
mochi-neko <t.o.e.4315@gmail.com>
2023-05-14 14:40:10 +0900

    Adjust accessibility

--- Assets/Mochineko/RelentStateMachine/StateResults.cs
+++ Assets/Mochineko/RelentStateMachine/StateResults.cs
@@ -3,7 +3,7 @@ using Mochineko.Relent.Result;
 
 namespace Mochineko.RelentStateMachine
 {
-    public static class StateResults
+    internal static class StateResults
     {
         public static ISuccessResult<IEventRequest<TEvent>> Succeed<TEvent>()
             => Results.Succeed(EventRequests.None<TEvent>());
